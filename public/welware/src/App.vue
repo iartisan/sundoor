90b4049fce64b516258d9d85eb2bc6e1b3a1ca14
mumuwooo <sevenbb2@sina.com>
2016-12-08 21:51:02 +0800

    baby fixed app-don

--- public/welware/src/App.vue
+++ public/welware/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-link to='/hello'>hello</router-link>
     <router-link to='/foo'>foo</router-link>
     <router-view></router-view>
@@ -14,12 +13,19 @@
 </script>
 
 <style>
+html, body{
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
